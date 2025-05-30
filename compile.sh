334932be4419b8fadda6e0c9a45185215874c611
nicojmn <nicolasjeanmenne@gmail.com>
2025-05-30 19:22:07 +0200

    include commit short hash in PDF

--- compile.sh
+++ compile.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function add_commit_hash() {
+  commit_hash=$(git rev-parse --short HEAD)
+  commit_date=$(git show -s --format=%ci HEAD | cut -d' ' -f1)
+  echo "\\newcommand{\\commitHash}{\\href{https://github.com/fnieri/LanguagesAndTranslatorsTP/tree/$commit_hash}{$commit_hash}}" > commit_hash.tex
+  echo "\\newcommand{\\commitDate}{$commit_date}" >> commit_hash.tex
+}
+
+add_commit_hash
 mkdir "out"
 pdflatex -output-directory=./out main.tex
 # Compile twice to get table of contents
